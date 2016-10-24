2fee44f6ed1396ae9a426e11c477d579c6e4c657
windleaf <417225444@qq.com>
2016-10-24 23:49:24 +0800

    android commit

--- app/src/main/java/com/yangyong/windleaf/learngit2/MainActivity.java
+++ app/src/main/java/com/yangyong/windleaf/learngit2/MainActivity.java
@@ -2,6 +2,7 @@ package com.yangyong.windleaf.learngit2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"android",Toast.LENGTH_SHORT).show();
     }
 }
